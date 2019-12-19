064b214172490a4a62612f2a6ca2aff3f2946f7a
kaviya06subramanian <58553109+kaviya06subramanian@users.noreply.github.com>
2019-12-19 17:04:44 +0530

    Create Length_of_array_without_sintax.java

---
+++ Length_of_array_without_sintax.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Length
+{
+  public static void main(String[] args)
+  {
+    int i;
+    Scanner sc=new Scanner(System.in);
+  int n,s=0;
+  System.out.println("ENTER THE RANGE");
+  n=sc.nextInt();
+  int[] a=new int[n];
+  
+ for(i=0;i<n;i++)
+  {
+  }
+  System.out.println("the length of array is "+i);
+  }
+
+}
